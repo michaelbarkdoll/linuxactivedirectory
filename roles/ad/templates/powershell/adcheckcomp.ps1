7f564c484ce97da8b8527c1b5cb4c44ea9537c4b
Michael Barkdoll <mabarkdoll@siu.edu>
2022-03-02 13:46:36 -0600

    Commit.

--- roles/ad/templates/powershell/adcheckcomp.ps1
+++ roles/ad/templates/powershell/adcheckcomp.ps1
@@ -1,1 +1,30 @@
----
+#
+# Determines if a computer exists at a particular Organization Unit inside Active Directory
+#
+# Script requires two arguments:
+# arg1: computerName (e.g., CS-533791)
+# arg2: SearchBase (e.g., "ou=Sample,dc=controller,dc=domain,dc=edu")
+
+import-module activedirectory
+
+$servername=$args[0]
+$searchbase=$args[1]
+
+$serverlist = @(
+    $servername
+)
+
+foreach ($server in $serverlist) {
+    try{
+        $output = Get-ADComputer -Filter "Name -eq '${server}'" -SearchBase $searchbase
+        if ($output -eq $Null) {
+            Write-Host "none"
+        }
+        else {
+            Write-Host "exists"  
+        }
+    }
+    catch{
+        Write-Host "none"
+    }
+}
