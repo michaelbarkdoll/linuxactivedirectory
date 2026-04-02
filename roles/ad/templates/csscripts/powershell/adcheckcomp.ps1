bef098f648cbd11dc175925beb3b5d7766e7a7ec
Michael Barkdoll <mabarkdoll@siu.edu>
2026-04-02 15:50:47 -0500

    Update adcheckcomp.ps1

--- roles/ad/templates/csscripts/powershell/adcheckcomp.ps1
+++ roles/ad/templates/csscripts/powershell/adcheckcomp.ps1
@@ -10,21 +10,51 @@ import-module activedirectory
 $servername=$args[0]
 $searchbase=$args[1]
 
-$serverlist = @(
-    $servername
-)
+function Test-InSearchBase {
+    param (
+        [string]$DistinguishedName,
+        [string]$SearchBase
+    )
 
-foreach ($server in $serverlist) {
-    try{
-        $output = Get-ADComputer -Filter "Name -eq '${server}'" -SearchBase $searchbase
-        if ($output -eq $Null) {
-            Write-Host "none"
-        }
-        else {
-            Write-Host "exists"  
-        }
+    if ([string]::IsNullOrWhiteSpace($DistinguishedName) -or [string]::IsNullOrWhiteSpace($SearchBase)) {
+        return $false
     }
-    catch{
-        Write-Host "none"
+
+    return $DistinguishedName.ToLower().EndsWith("," + $SearchBase.ToLower())
+}
+
+function Format-DistinguishedNames {
+    param (
+        [object[]]$Computers
+    )
+
+    return (($Computers | ForEach-Object { $_.DistinguishedName }) -join "|")
+}
+
+try {
+    $matches = @(Get-ADComputer -Filter "Name -eq '${servername}'" -Properties DistinguishedName -ErrorAction Stop)
+}
+catch {
+    $matches = @()
+}
+
+if ($matches.Count -eq 0) {
+    Write-Host "none"
+}
+elseif ($matches.Count -eq 1) {
+    if (Test-InSearchBase -DistinguishedName $matches[0].DistinguishedName -SearchBase $searchbase) {
+        Write-Host "exists_in_ou"
+}
+    else {
+        Write-Host "exists_in_other_ou:$($matches[0].DistinguishedName)"
+    }
+}
+else {
+    $matchesInBase = @($matches | Where-Object { Test-InSearchBase -DistinguishedName $_.DistinguishedName -SearchBase $searchbase })
+    if ($matchesInBase.Count -eq 1) {
+        Write-Host "multiple_matches:$((Format-DistinguishedNames -Computers $matches))"
+        }
+    else {
+        Write-Host "multiple_matches:$((Format-DistinguishedNames -Computers $matches))"
     }
 }
